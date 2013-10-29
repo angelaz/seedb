310842d937e07144501817b857465b7bfc2e247a
Angela Zhang <zhangela@mit.edu>
2013-10-28 21:08:38 -0400

    ran rails new

---
+++ seedb/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Seedb::Application.config.secret_key_base = '8f64f420af85767eb31956488188ec016aefb2aca54c8482ee0fc0cc24254f28504c1dd172214b28ceff4756c4cad2c2a153466268ae42e4f5a0e4818e8c31d8'
---
+++ seedb/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Seedb::Application.config.session_store :cookie_store, key: '_seedb_session'
