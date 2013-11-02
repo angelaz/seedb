8530ce8199d8d8f930bb8a7f15d3709d9e6c499f
Angela Zhang <zhangela@mit.edu>
2013-11-02 16:32:40 -0400

    wip

--- seedb/app/assets/javascripts/application.js
+++ seedb/app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require underscore-min
+//= requre backbone-min
 //= require_tree .
--- seedb/app/assets/javascripts/contributions.js
+++ seedb/app/assets/javascripts/contributions.js
@@ -55,3 +55,48 @@ function drawChart(chartType, counts, title, x_axis, y_axis, width, height) {
         chart.draw(data, options);
     });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var GraphView = Backbone.View.extend({
+
+//   tagName: "li",
+
+//   className: "document-row",
+
+//   events: {
+//     "click .icon":          "open",
+//     "click .button.edit":   "openEditDialog",
+//     "click .button.delete": "destroy"
+//   },
+
+//   initialize: function() {
+//     this.listenTo(this.model, "change", this.render);
+//   },
+
+//   render: function() {
+//     ...
+//   }
+
+// });
+
+// var graph_view = new GraphView();
+
+
+
+
+
+
+
