04c4cbd87d8cd185aaed32bb9e3d4b2ef542bdb2
Angela Zhang <zhangela@mit.edu>
2013-11-02 19:51:36 -0400

    added params

--- seedb/app/assets/javascripts/contributions.js
+++ seedb/app/assets/javascripts/contributions.js
@@ -1,6 +1,5 @@
-// # Place all the behaviors and hooks related to the matching controller here.
-// # All this logic will automatically be available in application.js.
-// # You can use CoffeeScript in this file: http://coffeescript.org/
+var ChartModel = Backbone.Model.extend({});
+var model = new ChartModel();
 
 // Load the Visualization API and the piechart package.
 google.load('visualization', '1.0', {'packages':['corechart']});
@@ -9,16 +8,12 @@ google.load('visualization', '1.0', {'packages':['corechart']});
 google.setOnLoadCallback(fetchData);
 
 function fetchData() {
-    $.get("/contributions.json", {}, organizeData, "json");
+    $.get("/contributions.json", {}, prepareChart, "json");
 }
 
-function organizeData(data) {
+function prepareChart(data) {
+
     var counts = {};
-    var x_axis = "Occupations";
-    var y_axis = "Number of Donors";
-    var title = "Contributor Occupations";
-    var width = 600;
-    var height = 500;
 
     data.forEach(function(contribution) {
         if (! counts[contribution.contbr_occupation]) {
@@ -28,51 +23,69 @@ function organizeData(data) {
         }
     });
 
-    drawChart("ColumnChart", counts, title, x_axis, y_axis, width, height);
+    model.set("counts", counts);
+    model.set("x_axis", "Occupations");
+    model.set("y_axis", "Number of Donors");
+    model.set("title", "Contributor Occupations");
+    model.set("width", 600);
+    model.set("height", 500);
+    model.set("div_name", "chart_div");
+    model.set("chart_type", "ColumnChart");
+
+    drawChart();
 }
+
 // Callback that creates and populates a data table,
 // instantiates the pie chart, passes in the data and
 // draws it.
-function drawChart(chartType, counts, title, x_axis, y_axis, width, height) {
+function drawChart() {
 
     // Create the data table.
     var data = new google.visualization.DataTable();
-    data.addColumn('string', x_axis);
-    data.addColumn('number', y_axis);
-    data.addRows(_.pairs(counts));
+    data.addColumn('string', model.get("x_axis"));
+    data.addColumn('number', model.get("y_axis"));
+    data.addRows(_.pairs(model.get("counts")));
 
     // Set chart options
-    var options = {'title': title,
-                   'width':width,
-                   'height':height};
+    var options = {'title': model.get("title"),
+                   'width':model.get("width"),
+                   'height':model.get("height")};
 
     // Instantiate and draw our chart, passing in some options.
 
-    var chart = new google.visualization[chartType](document.getElementById('chart_div'));
+    var chart = new google.visualization[model.get("chart_type")](document.getElementById(model.get("div_name")));
 
     $(document).ready(function() {
         chart.draw(data, options);
     });
 }
 
-SearchView = Backbone.View.extend({
+// var ChartModel = Backbone.Model.extend({});
+
+
+GraphView = Backbone.View.extend({
     initialize: function(){
         this.render();
     },
     render: function(){
-        var template = _.template( $("#search_template").html(), {} );
+        var template = _.template( $("#render_template").html(), {} );
         this.$el.html( template );
     },
     events: {
-        "click input[type=button]": "doSearch"  
+        "click input[id=render_button]": "doRender"  
     },
-    doSearch: function(){
-        // Button clicked
-        console.log(this);
+    doRender: function(){
+        model.set("chart_type", $("#chart_type_input").val());
+        model.set("title", $("#title_input").val());
+        model.set("x_axis", $("#x_axis_input").val());
+        model.set("y_axis", $("#y_axis_input").val());
+        model.set("width", $("#width_input").val());
+        model.set("height", $("#height_input").val());
+        drawChart();      
     }
 });
 
-var search_view = new SearchView({ el: $("#search_container") });
+var graph_view = new GraphView({ el: $("#render_container") });
 
 
 
