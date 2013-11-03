6e4ba64eec2a64b87f728bbc724c1079f546cd47
Angela Zhang <zhangela@mit.edu>
2013-11-03 01:44:00 -0500

    added aggregate by

--- seedb/app/assets/javascripts/contributions.js
+++ seedb/app/assets/javascripts/contributions.js
@@ -12,9 +12,10 @@ var ChartModel = Backbone.Model.extend({
         counts: null,
         div_name: "chart_div",
         chart_type: "ColumnChart",
+        aggregate_by: "contbr_occupation",
         title: "Contributor Occupations",
-        x_axis: "Occupations",
-        y_axis: "Number of Contributors",
+        x_axis_label: "Occupations",
+        y_axis_label: "Number of Contributors",
         width: 600,
         height: 500
       };
@@ -22,7 +23,7 @@ var ChartModel = Backbone.Model.extend({
 });
 
 var model = new ChartModel();
-model.on("change", drawChart);
+model.on("change:aggregate_by", prepareChart);
 
 
 // Backbone View
@@ -39,12 +40,14 @@ GraphView = Backbone.View.extend({
         "click input[id=render_button]": "doRender"  
     },
     doRender: function(){
-        model.set("chart_type", $("#chart_type_input").val());
-        model.set("title", $("#title_input").val());
-        model.set("x_axis", $("#x_axis_input").val());
-        model.set("y_axis", $("#y_axis_input").val());
-        model.set("width", $("#width_input").val());
-        model.set("height", $("#height_input").val());
+
+        // model.set("chart_type", $("#chart_type_input").val());
+        // model.set("title", $("#title_input").val());
+        model.set("aggregate_by", $("#aggregate_by_input").val());
+        // model.set("x_axis_label", $("#x_axis_label_input").val());
+        // model.set("y_axis_label", $("#y_axis_label_input").val());
+        // model.set("width", $("#width_input").val());
+        // model.set("height", $("#height_input").val());
     }
 });
 
@@ -53,46 +56,65 @@ var graph_view = new GraphView({ el: $("#render_container") });
 
 // Draw Chart Google Charts 
 
-google.load('visualization', '1.0', {'packages':['corechart']});
-google.setOnLoadCallback(fetchData);
+//Temporary:
+// google.load('visualization', '1.0', {'packages':['corechart']});
+// google.setOnLoadCallback(fetchData);
+window.onload = function() {
+    fetchData();
+};
+
 
 function fetchData() {
-    $.get("/contributions.json", {}, prepareChart, "json");
+    console.log("fetchData called.");
+    $.get("/contributions.json", prepareChart, "json");
 }
 
-function prepareChart(data) {
+function prepareChart(data_fetched) {
+
+    console.log("prepareChart called.");
+    
+    if (_.isArray(data_fetched)) {
+        model.set("data_fetched", data_fetched);
+        var data = data_fetched;
+    } else {
+        var data = model.get("data_fetched");
+    }
 
     var counts = {};
+    var aggregate_by = model.get("aggregate_by");
 
     data.forEach(function(contribution) {
-        if (! counts[contribution.contbr_occupation]) {
-            counts[contribution.contbr_occupation] = 1;
+        if (! counts[contribution[aggregate_by]]) {
+            counts[contribution[aggregate_by]] = 1;
         } else {
-            counts[contribution.contbr_occupation] += 1;
+            counts[contribution[aggregate_by]] += 1;
         }
     });
 
+    console.log(counts);
     model.set("counts", counts);
 }
 
 function drawChart() {
 
-    // Create the data table.
-    var data = new google.visualization.DataTable();
-    data.addColumn('string', model.get("x_axis"));
-    data.addColumn('number', model.get("y_axis"));
-    data.addRows(_.pairs(model.get("counts")));
+    console.log("drawChart called.");
+    console.log(model.attributes);
+    // // Create the data table.
+    // var data = new google.visualization.DataTable();
+    // data.addColumn('string', model.get("x_axis_label"));
+    // data.addColumn('number', model.get("y_axis_label"));
+    // data.addRows(_.pairs(model.get("counts")));
 
-    // Set chart options
-    var options = {'title': model.get("title"),
-                   'width':model.get("width"),
-                   'height':model.get("height")};
+    // // Set chart options
+    // var options = {'title': model.get("title"),
+    //                'width':model.get("width"),
+    //                'height':model.get("height")};
 
-    // Instantiate and draw our chart, passing in some options.
+    // // Instantiate and draw our chart, passing in some options.
 
-    var chart = new google.visualization[model.get("chart_type")](document.getElementById(model.get("div_name")));
+    // var chart = new google.visualization[model.get("chart_type")](document.getElementById(model.get("div_name")));
 
-    $(document).ready(function() {
-        chart.draw(data, options);
-    });
+    // $(document).ready(function() {
+    //     chart.draw(data, options);
+    // });
 }
